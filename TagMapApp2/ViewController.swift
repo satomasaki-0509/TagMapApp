964eab7b277c54bf8321ed235e002a25b46f8c2d
satomasaki-0509 <masakiti59@gmail.com>
2021-04-27 22:27:29 +0900

    Initial Commit

---
+++ TagMapApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TagMapApp2
+//
+//  Created by Masaki Sato on 2021/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
